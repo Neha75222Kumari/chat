ebb53b8bc2c8f6580776afe12707bad4eecb8746
Neha75222Kumari <94526649+Neha75222Kumari@users.noreply.github.com>
2021-12-15 01:23:55 +0530

    Create ArrayAdapter.java

---
+++ ArrayAdapter.java
@@ -0,0 +1,6 @@
+private boolean sendChatMessage() {
+        chatArrayAdapter.add(new ChatMessage(side, chatText.getText().toString()));
+        chatText.setText("");
+        side = !side;
+        return true;
+    }
